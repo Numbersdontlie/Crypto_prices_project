7606d60c05563f67510b36eb6d6a7f7173253952
Luis Perez <luifer92@gmail.com>
2022-04-02 02:37:57 +0200

    uploading extract, transform and load files to project

--- Crypto_prices_project/load_data.sh
+++ Crypto_prices_project/load_data.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
 
+#Delete all the files in the folder
+#aws s3 rm s3://crypto-price-project --recursive
+
+#Upload the files recursively to my bucket 
 aws s3 cp /home/luis/code/Numbersdontlie/Crypto_prices_project/raw_data s3://crypto-price-project --recursive
